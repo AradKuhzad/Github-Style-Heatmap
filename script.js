e4d9592c5fd9a0ae50dbf2a0366e568869f90196
Arad <aradkuhzad@gmail.com>
2026-06-03 22:59:42 +0330

    Implement data generation and heatmap rendering logic in script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function generateData() {
     let inactiveStreak = 0;
     let isInactive = 0;
     const activities = [];
+    for (let day = 1; day <= 365; day++) {
+        let score = Math.floor(Math.random() * 11);
+        activities.push({
+            day: day,
+             score: score});
